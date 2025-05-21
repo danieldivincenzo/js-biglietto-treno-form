1ce94bdb7f908aea396d6c4214930181f9dbe38d
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-21 11:51:53 +0200

    modificato form e aggiunto human code in js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// 1. Leggo i km che inserisce l'utente
+// 2. Leggo il tipo di sconto che devo applicare
+// 3. Calcolo il prezzo del biglietto e applico lo sconto se necessario
+// 4. Stampo il prezzo del biglietto
