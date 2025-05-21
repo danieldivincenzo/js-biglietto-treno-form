61cf71567c35508e983e4c48147cc521625882bf
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-05-21 12:21:34 +0200

    inserito btn e div per output e letti valori di input utente

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@
 // 2. Leggo il tipo di sconto che devo applicare
 // 3. Calcolo il prezzo del biglietto e applico lo sconto se necessario
 // 4. Stampo il prezzo del biglietto
+
+const inputChilometri = document.getElementById("chilometri")
+const selectSconto = document.getElementById("sconto")
+const buttonCalcola = document.getElementById("calcola")
+const divPrezzoBiglietto = document.getElementById("prezzo-biglietto")
+console.log(inputChilometri, selectSconto, buttonCalcola, divPrezzoBiglietto)
+
+document.addEventListener("click", function () {
+    const chilometri = inputChilometri.value
+    const sconto = selectSconto.value
+    const prezzoPerKm = 0.21
+
+})
